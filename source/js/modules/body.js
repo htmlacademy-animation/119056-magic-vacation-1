4180047e4d9ad9c01b66c800b8525d90a2ffe527
Evgenii Menshikov <menshikov@uchi.ru>
2020-05-05 02:55:44 +0500

    * use backticks

--- source/js/modules/body.js
+++ source/js/modules/body.js
@@ -1,7 +1,7 @@
 export default () => {
   const markBodyAsLoad = () => {
-    document.body.classList.add('body-load');
-    window.removeEventListener('load', markBodyAsLoad, false);
+    document.body.classList.add(`body-load`);
+    window.removeEventListener(`load`, markBodyAsLoad, false);
   };
-  window.addEventListener('load', markBodyAsLoad, false);
+  window.addEventListener(`load`, markBodyAsLoad, false);
 };
