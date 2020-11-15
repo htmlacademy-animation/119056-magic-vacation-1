2ec507f801e514b64c777dd575b9ad89cadbeed4
Evgenii Menshikov <menshikov@uchi.ru>
2020-11-15 14:24:27 +0500

    * add listener for rules title - remove animation class from button

--- source/js/modules/rules.js
+++ source/js/modules/rules.js
@@ -1,8 +1,10 @@
 export default () => {
-  const showButtonAfterLastRulesItem = () => {
+  const toggleRulesButton = (force) => {
     const rulesLink = document.querySelector(`.rules__link`);
-    rulesLink.classList.add(`rules__link--visible`);
+    rulesLink.classList.toggle(`rules__link--visible`, force);
   };
+  const rulesTitle = document.querySelector(`.rules__title`);
   const lastRulesItem = document.querySelector(`.rules__item:last-child p`);
-  lastRulesItem.addEventListener(`animationstart`, showButtonAfterLastRulesItem, false);
+  rulesTitle.addEventListener(`animationstart`, () => toggleRulesButton(false), false);
+  lastRulesItem.addEventListener(`animationstart`, () => toggleRulesButton(true), false);
 };
